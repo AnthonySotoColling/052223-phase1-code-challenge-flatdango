9fa4bbc2c9d94eeb72025afe911d684a14313f23
anthonyLuisSotoColling <govips@gmail.com>
2023-06-01 10:31:20 -0700

    did not spend time wizly

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// Your code here
+const moiveAPI='http://localhost:3000/films'
+const movieDetails=document.getElementById('showing')
+
+fetch(moiveAPI)
+    .then(res => res.json())
+    .then(renderMoives)
+
+function renderMoives(moives) {
+    moives.forEach(renderMoivesDetail)
+}
+function renderMoivesDetail(movies) {
+    const moviePic=movieDetails.queryselector('.poster')
+    moviePic.src=movies.poster
+    moviePic.alt=movies.title
+    movieDetails.queryselector('.runtime').textContent=movies.runtime
+    movieDetails.queryselector('.showtime').textContent=movies.showtime
+    //capacity - tickets_sold = availableTickets
+}
